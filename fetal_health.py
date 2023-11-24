96a40893ccc046f7b47b1bed5c774dc6a8237cb2
Katherine Chan <kchan78@calpoly.edu>
2023-11-24 01:57:01 -0800

    Fixed pickling

--- fetal_health.py
+++ fetal_health.py
@@ -5,7 +5,8 @@
 import streamlit as st
 import pandas as pd
 import pickle
-import sklearn
+import warnings
+warnings.filterwarnings('ignore')
 
 st.title('Fetal Health Classification: A Machine Learning App') 
 
@@ -14,23 +15,23 @@ st.image('fetal_health_image.gif', width = 650)
 
 st.subheader("This machine learning application uses multiple inputs to predict fetal health classifications. ")
 st.write("Upload your dataset to get started! Please ensure your data strictly adheres to the specified format, shown below:") 
+
+# Reading the pickle files that we created before 
+# Random Forest
+rf_pickle = open('rf_fetal.pickle', 'rb') 
+rf_model = pickle.load(rf_pickle) 
+rf_pickle.close()
+# Map file
+map_pickle = open('output_fetal.pickle', 'rb') 
+unique_fetal_mapping = pickle.load(map_pickle) 
+map_pickle.close()
+
 # Load and display original dataset
 original_df = pd.read_csv('fetal_health.csv')
 st.dataframe(original_df.head())
 
 fetal_health_file = st.file_uploader('Upload your own fetal health data')
 
-# Reading the pickle files that we created before 
-# Random Forest
-rf_pickle = open('rf_fetal.pickle', 'rb') 
-# Map file
-map_pickle = open('output_fetal.pickle', 'rb') 
-rf_cv = pickle.load(rf_pickle) 
-rf_model = rf_cv.best_estimator_
-unique_fetal_mapping = pickle.load(map_pickle) 
-rf_model = pickle.load(rf_pickle)
-map_pickle.close()
-rf_pickle.close()
 
 # color function definition 
 # NOTE: Code from https://www.geeksforgeeks.org/highlight-pandas-dataframes-specific-columns-using-applymap/
